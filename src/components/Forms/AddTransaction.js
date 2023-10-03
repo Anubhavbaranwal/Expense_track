4542be8b9a0938f8244fbc7def64e61c847d7ad7
Anubhavbaranwal <anubhavbaranwal08@gmail.com>
2023-10-03 09:05:32 +0530

    accountSlice change

--- src/components/Forms/AddTransaction.js
+++ src/components/Forms/AddTransaction.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import { createTransactionAction } from "../../components/Redux/aadTransactions";
+import { createTransactionAction } from "../../redux/slice/transactions/transactionSlice";
 
 const AddTransaction = () => {
   //get the account ID
